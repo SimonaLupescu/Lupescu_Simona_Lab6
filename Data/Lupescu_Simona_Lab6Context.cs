b658959b2703c4eff44ffb33e84cbe6baa1b2f4a
Simona Lupescu <simonalupescu23@gmail.com>
2021-01-02 20:00:19 +0200

    Add project files.

---
+++ Data/Lupescu_Simona_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lupescu_Simona_Lab6.Models;
+
+namespace Lupescu_Simona_Lab6.Data
+{
+    public class Lupescu_Simona_Lab6Context : DbContext
+    {
+        public Lupescu_Simona_Lab6Context (DbContextOptions<Lupescu_Simona_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lupescu_Simona_Lab6.Models.Book> Book { get; set; }
+    }
+}
